f6487e1d7fb5a28878f7048e1316088657536468
Bernat Moix <bernat@verse.me>
2018-03-21 11:03:02 +0100

    Update font used to generate images

--- code.php
+++ code.php
@@ -155,7 +155,7 @@ $height = round( $height, 3 );
 $text_angle = 0;
 
  // If you want to use a different font simply upload the true type font (.ttf) file to the same directory as this PHP file and set the $font variable to the font file name. I'm using the M+ font which is free for distribution -> http://www.fontsquirrel.com/fonts/M-1c
-$font = 'fonts/mplus-2c-light.ttf';
+$font = 'fonts/SF-UI-Text-Regular.otf';
 
 // Create an image
 $img = imageCreate( $width, $height );
